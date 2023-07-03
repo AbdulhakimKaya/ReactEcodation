2d7cd2ade9fdb6071d89141b34e089edb0eff55f
Abdulhakim KAYA <abdulhakimkaya2199@gmail.com>
2023-07-04 00:26:51 +0300

    Search, Dark Mode, Header, Footer

--- template.js
+++ template.js
@@ -1,1 +1,26 @@
 // alert("example")
+
+// Dark Mode
+let darkMode = () => {
+    document.body.classList.toggle("dark_mode")
+}
+
+// input search
+$(document).ready(function() {
+    const searchApi = ["Adana", "Balıkesir", "Çorum", "Denizli", "Diyarbakır", "Elazığ", "Malatya", "Van"]
+    $( '#tags' ).autocomplete({ 
+    source: searchApi 
+    });
+})
+
+// Footer Date
+let newDate = () => {
+    const date = new Date().getFullYear();
+
+    // JS Dom
+    // document.getElementById("date_id").innerHTML = date;
+
+    // JQuery Dom
+    $("#date_id").html(date)
+}
+newDate();
