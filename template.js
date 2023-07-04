33a0344707fd955acce8c87e8ac442231708e6dd
Abdulhakim KAYA <abdulhakimkaya2199@gmail.com>
2023-07-04 11:40:50 +0300

    Back to Top updated

--- template.js
+++ template.js
@@ -24,3 +24,21 @@ let newDate = () => {
     $("#date_id").html(date)
 }
 newDate();
+
+let mybutton = document.getElementById("back_top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
